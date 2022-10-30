b514e38998459c46f6e5b2ff3b6237d9575f4664
sagar <skatahre@gmail.com>
2022-10-31 01:48:35 +0530

    program to check User Last Name is validate or not

--- Pattern.cs
+++ Pattern.cs
@@ -9,12 +9,12 @@ using System.Security.Cryptography.X509Certificates;
 namespace regex
 {
     public class UserRegistration
-    {
-        //regex(regular expression) to check word
+    {//regex(regular expression) to check word
         public static string name = "^[A-Z][A-za-z]{2,}";
 
         public static bool validate(string word)
         {
+
             return Regex.IsMatch(word, name);
 
         }
