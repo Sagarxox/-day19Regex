d89217796ee138dbe9504304de93b0f46e6b34d5
sagar <skatahre@gmail.com>
2022-10-31 01:31:27 +0530

    program to check User First Name is validate or not

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\tWelcome To User Registration Problem");
+
+            Console.Write("\nEnter The First Name:");
+            String fname = Console.ReadLine();
+
+            if (UserRegistration.validate(fname))
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+            Console.ReadLine();
+        }
+    }
+}
