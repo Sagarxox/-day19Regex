4932abd4c12b8918c46f1f31e2f2104b8f4ebb5c
sagar <skatahre@gmail.com>
2022-10-31 01:31:41 +0530

    program to check User First Name is validate or not

---
+++ Pattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace regex
+{
+    public class UserRegistration
+    {
+        //regex(regular expression) to check word
+        public static string name = "^[A-Z][A-za-z]{2,}";
+
+        public static bool validate(string word)
+        {
+            return Regex.IsMatch(word, name);
+
+        }
+    }
+}
