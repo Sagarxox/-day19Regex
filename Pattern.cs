9acd5c03530b3beb7fdd19d3c1fc6dc9e86eb637
sagar <skatahre@gmail.com>
2022-10-31 01:56:38 +0530

    program to check Userr mobile number  is validate or not

--- Pattern.cs
+++ Pattern.cs
@@ -9,8 +9,8 @@ using System.Security.Cryptography.X509Certificates;
 namespace regex
 {
     public class UserRegistration
-    {//regex(regular expression) to check word
-        public static string name = "^[A-Z][A-za-z]{2,}";
+    {  //regex(regular expression) as we need.
+        public static string name = "^[91]{2,2}[\\s][1-9]{10,10}";
 
         public static bool validate(string word)
         {
