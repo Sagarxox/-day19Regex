aaccc533abf45aeba9ae1babf1390727727a76d7
sagar <skatahre@gmail.com>
2022-12-22 23:35:28 +0530

    Check password validation for rule2

--- Pattern.cs
+++ Pattern.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace UC_5_PasswordRule1
+namespace UC_6_PasswordRule2
 {
     public class Pattern
     {
-        public static string REGEX_CODE = "^[a-z]{8}$";
+        public static string REGEX_CODE = "^[a-zA-Z]{8}$";
         public bool Validate_Password(string password)
         {
             return Regex.IsMatch(password, REGEX_CODE);
