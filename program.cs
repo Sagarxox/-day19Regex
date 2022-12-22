bd4e175d8741d732fd68ff9951306f6b2a5263ac
sagar <skatahre@gmail.com>
2022-12-22 23:31:52 +0530

    Check password validation for rule1

---
+++ program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace UC_5_Password_Rule1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pat = new Pattern();
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+            bool num = pat.Validate_Password(password);
+            Console.WriteLine(num);
+            if (num)
+            {
+                Console.WriteLine("correct match");
+            }
+            else
+            {
+                Console.WriteLine("Wrong match");
+            }
+        }
+    }
+}
